1b9475482865aef0fbe33ba12f2653f9c23ae3ea
Hisham <hisham.dbd@gmail.com>
2018-09-13 18:08:26 +0530

    Testing Jitpack

---
+++ library/hls/src/main/java/com/google/android/exoplayer2/source/hls/hisham/Hisham.java
@@ -0,0 +1,13 @@
+package com.google.android.exoplayer2.source.hls.hisham;
+
+/**
+ * Created by Hisham on 13/Sep/2018 - 18:04
+ */
+public class Hisham {
+
+
+    public int foo(){
+        return 10;
+    }
+
+}
